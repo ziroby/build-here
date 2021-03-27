0758494be1d64b96539824b56c04b02ed070a2d5
Ron Ziroby Romero <ziroby@ziroby.com>
2021-03-27 16:11:39 +0000

    Run the command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::process;
+use std::process::{Command, Stdio};
 
 fn main() {
     let command;
@@ -7,10 +8,23 @@ fn main() {
     if Path::new("./gradlew").exists() {
         command = "./gradlew";
         default = "build";
+    } else if Path::new("./Cargo.toml").exists() {
+        command = "cargo";
+        default = "build";
     } else {
         eprintln!("Unknown build directory");
         process::exit(1);
     }
 
     println!("Running {} {}", command, default);
+
+    let mut process = Command::new(command)
+        .args(default.split(' '))
+        .stdin(Stdio::null())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .expect("Failed to start external program");
+
+    process.wait().expect("Error from build process");
 }
